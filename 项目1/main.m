39008dfa8b3b28461e26e9bc0ab060ccbbcd6431
黄家辉 <afrid@huangjiahuidemacbook-pro.local>
2016-04-09 19:14:23 +0800

    Initial Commit

---
+++ 项目1/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  项目1
+//
+//  Created by 黄家辉 on 16/4/9.
+//  Copyright © 2016年 黄家辉. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
